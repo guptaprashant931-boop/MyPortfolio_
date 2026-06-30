1ec752c0a1e03e7c77704c6d9891de76354763f0
guptaprashant931-boop <guptaprashant931@gmail.com>
2026-06-30 16:22:18 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -85,7 +85,7 @@ MIDDLEWARE = [
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://guptaprashant931-boop.github.io/MyPortfolio_/",  
+    "https://guptaprashant931-boop.github.io",  
 ]
 
 ALLOWED_HOSTS = [
